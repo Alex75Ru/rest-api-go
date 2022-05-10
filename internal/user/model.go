a1d896ec9ff7628a941b0e949ae38a281a5920c0
Alex <Asutp.org@gmail.com>
2022-05-11 02:59:46 +0900

    mongodb

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,1 +1,16 @@
 package user
+
+type User struct {
+	// bson _id - _ид - ключ индекс с автогенерацией   omitempty - может быть пустым
+	// поле с json: "-" не будет выдаваться
+	ID           string `json:"id" bson:"_id,omitempty"`
+	Username     string `json:"username" bson:"username"`
+	PasswordHash string `json:"-" bson:"password"`
+	Email        string `json:"email" bson:"email"`
+}
+
+type CreateUserDTO struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
